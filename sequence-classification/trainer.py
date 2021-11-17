d6b4696caa7e98d74f9142af56d702438e68bfaa
Chenchao Zhao <chenchao.zhao@clarifai.com>
2021-11-17 14:21:33 +0800

    tokenizer

--- sequence-classification/trainer.py
+++ sequence-classification/trainer.py
@@ -13,6 +13,11 @@ def load_dataset(path_to_yaml: str) -> hfd.DatastDict:
     return build_dataset_dict_from_config(cfg)
 
 
+def load_tokenizer(path_to_yaml: str) -> hf.AutoTokenizer:
+    cfg = load_yaml(path_to_yaml)
+    return hf.AutoTokenizer.from_pretrained(**cfg["tokenizer"])
+
+
 def load_model(path_to_yaml: str) -> torch.nn.Module:
     cfg = load_yaml(path_to_yaml)
     if "device" in cfg:
