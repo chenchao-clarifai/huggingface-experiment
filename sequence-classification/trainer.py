aba0b961b3d48537c831882590aa53d745354751
Chenchao Zhao <chenchao.zhao@clarifai.com>
2021-11-17 22:48:36 +0800

    add main

--- sequence-classification/trainer.py
+++ sequence-classification/trainer.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 import datasets as hfd
 import torch
 import transformers as hf
@@ -8,18 +10,15 @@ from ..utils.dataset import build_dataset_dict_from_config
 DEVICE = "cuda:0"
 
 
-def load_dataset(path_to_yaml: str) -> hfd.DatastDict:
-    cfg = load_yaml(path_to_yaml)
+def load_dataset(cfg: Dict) -> hfd.DatastDict:
     return build_dataset_dict_from_config(cfg)
 
 
-def load_tokenizer(path_to_yaml: str) -> hf.AutoTokenizer:
-    cfg = load_yaml(path_to_yaml)
-    return hf.AutoTokenizer.from_pretrained(**cfg["tokenizer"])
+def load_tokenizer(cfg: Dict) -> hf.AutoTokenizer:
+    return hf.AutoTokenizer.from_pretrained(**cfg)
 
 
-def load_model(path_to_yaml: str) -> torch.nn.Module:
-    cfg = load_yaml(path_to_yaml)
+def load_model(cfg: Dict) -> torch.nn.Module:
     if "device" in cfg:
         device = cfg.pop("device")
     else:
@@ -27,3 +26,48 @@ def load_model(path_to_yaml: str) -> torch.nn.Module:
     model = hf.AutoModelForSequenceClassification.from_pretrained(**cfg)
     model.to(device)
     return model
+
+
+def main(yaml_config_path: str) -> None:
+
+    cfg = load_yaml(yaml_config_path)
+
+    dataset_cfg = cfg["dataset_config"]
+    model_cfg = cfg["model_config"]
+    tokenizer_cfg = dataset_cfg["tokenizer"]
+    trainer_cfg = cfg["trainer_config"]
+
+    model = load_model(model_cfg)
+    tokenizer = load_tokenizer(tokenizer_cfg)
+
+    dataset_dict = load_dataset(dataset_cfg)
+    if "train" in dataset_dict:
+        train_dataset = dataset_dict["train"]
+    else:
+        train_dataset = None
+
+    if "eval" in dataset_dict:
+        eval_dataset = dataset_dict["eval"]
+    else:
+        eval_dataset = None
+
+    training_args = hf.TrainingArguments(**trainer_cfg)
+    trainer = hf.Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        tokenizer=tokenizer,
+    )
+    trainer.train()
+    trainer.save_model()
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--yaml_config_path", type=str, nargs=None)
+    kwargs = vars(parser.parse_args())
+
+    main(**kwargs)
